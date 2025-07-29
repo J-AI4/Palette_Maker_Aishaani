a8e6027b35af7be2d2b360627985426140c353e0
J.AI <aishaani.jana@gmail.com>
2025-07-29 14:29:59 -0400

    modified script.js

--- script.js
+++ script.js
@@ -9,4 +9,37 @@ imageInput.addEventListener('change', () = {
 
     const img = new Image();
     const reader = new FileReader();
+
+    reader.onload = (e) => {
+        img.src = e.target.result;
+    }
+
+    img.onload = () => {
+        const maxSize = 200;
+        let width = img.width;
+        let height = img.height;
+
+        if (width > height) {
+            if (width > maxSize) {
+                height = Math.round((height * maxSize) / width);
+                width = maxSize;
+            }
+        } else {
+            if (height > maxSize) {
+                width = Math.round((width * maxSize) / height);
+                height = maxSize;
+            }
+        }
+
+        canvas.width = width;
+        canvas.height = height;
+        ctx.drawImage(img, 0, 0, width, height);
+
+        const imageData = ctx.getImageData(0, 0, width, height);
+        const colors = getDominantColors(imageData, 4);
+
+        displayPalette(colors);
+    };
+
+
 })
