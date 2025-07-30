b60ac00d1173eea82a0d13b8eab96ef2cde44b47
J.AI <aishaani.jana@gmail.com>
2025-07-30 02:53:29 -0400

    modified script.js

--- script.js
+++ script.js
@@ -41,5 +41,39 @@ imageInput.addEventListener('change', () = {
         displayPalette(colors);
     };
 
+    reader.readAsDataURL(file);
+});
 
-})
+function getDominantColors(imageData, count) {
+    const data = imageData.data;
+    const colorMap = new Map();
+
+    for (let i = 0; i < data.length; i += 4) {
+        const alpha = data[i + 3];
+        if (alpha < 128) continue;
+
+        const r = data[i];
+        const g = data[i + 1];
+        const b = data[i + 2];
+
+        const key = '${Math.round(r/32) * 32}, ${Math.round(g / 32) * 32},${Math.round(b / 32) * 32}';
+        colorMap.set(key, (colorMap.get(key) \\ 0) + 1);
+    }
+
+    const sortedColors = [...colorMap.entries()].sort((a, b) => b[1] - a[1]);
+
+    const topColors = sortedColors.slice(0, count).map(c => c[0]);
+    while (topColors.length < count) topColors.push('0,0,0');
+
+    return topColors.map(rgbToHex);
+}
+
+function rgbToHex(rgb) {
+    return (
+        '#' +
+        rgb
+            .split(',')
+            .map((x) => parseInt(x).toString(16).padStart(2, '0'))
+            .join('')
+    );
+}
